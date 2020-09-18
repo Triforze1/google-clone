efba839dfddb62590c3f9327ac628b965209f556
Bryan <b_farrill@ymail.com>
2020-09-18 18:24:33 -0400

    Initial Commit

---
+++ google-clone/src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../css/Home.css'
+
+function Home() {
+    return (
+        <div className='home'>
+            <h1>This is home page</h1>
+
+            <div className='home_header'>
+                <div className="home_headerLeft">
+                    
+                </div>
+                <div className="home_headerRight">
+
+                </div>
+            </div>
+
+            <div className='home_body'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
